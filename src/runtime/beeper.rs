2d56e3ca4b28fc702bce67385c3e3fa0343a0496
Anton Tarasov <namtarr123@gmail.com>
2026-06-20 11:07:45 +0700

    Hide dropped sink log from release builds

--- src/runtime/beeper.rs
+++ src/runtime/beeper.rs
@@ -1,5 +1,7 @@
 use rodio::{DeviceSinkBuilder, MixerDeviceSink, Player, Source, source::SineWave};
 
+const IS_DEBUG: bool = cfg!(debug_assertions);
+
 pub struct Beeper {
     player: Player,
     _sink: MixerDeviceSink,
@@ -9,11 +11,13 @@ impl Beeper {
     pub fn new() -> Beeper {
         let source = SineWave::new(440.0).amplify(0.20).repeat_infinite();
 
-        let sink = DeviceSinkBuilder::open_default_sink().expect("default audio stream");
+        let mut sink = DeviceSinkBuilder::open_default_sink().expect("default audio stream");
         let player = Player::connect_new(&sink.mixer());
 
         player.pause();
         player.append(source);
+
+        sink.log_on_drop(IS_DEBUG);
 
         Beeper {
             player: player,
